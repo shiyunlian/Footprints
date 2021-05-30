c69c673c9b8de35369169b88b0a261455986d3cc
shiyunlian <shiyunlian00@gmail.com>
2021-05-30 13:53:23 -0700

    add submit and clear buttons

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
+import FileBase from "react-file-base64";
 import useStyles from "./styles";
 
 const Form = () => {
@@ -12,6 +13,7 @@ const Form = () => {
   });
   const classes = useStyles();
   const handleSubmit = () => {};
+  const clear = () => {};
   return (
     <Paper className={classes.paper}>
       <form
@@ -31,6 +33,57 @@ const Form = () => {
             setPostData({ ...postData, creator: e.target.value })
           }
         />
+        <TextField
+          name="title"
+          varient="outlined"
+          label="Title"
+          fullWidth
+          value={postData.title}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+        />
+        <TextField
+          name="message"
+          varient="outlined"
+          label="Message"
+          fullWidth
+          value={postData.message}
+          onChange={(e) =>
+            setPostData({ ...postData, message: e.target.value })
+          }
+        />
+        <TextField
+          name="tags"
+          varient="outlined"
+          label="Tags"
+          fullWidth
+          value={postData.tags}
+          onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+        />
+        <div className={classes.fileInput}>
+          <FileBase>
+            type="file" multiple={false} onDone=
+            {({ base64 }) => setPostData({ ...postData, selectedFile: base64 })}
+          </FileBase>
+          <Button
+            className={classes.buttonSubmit}
+            varient="container"
+            color="primary"
+            size="large"
+            type="submit"
+            fullWidth
+          >
+            Submit
+          </Button>
+          <Button
+            varient="contained"
+            color="secondary"
+            size="large"
+            onClick={clear}
+            fullWidth
+          >
+            Clear
+          </Button>
+        </div>
       </form>
     </Paper>
   );
