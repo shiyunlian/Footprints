5faa23919b8b8c2d0ff9fc361028807d20e8c599
shiyunlian <shiyunlian00@gmail.com>
2021-05-29 22:51:16 -0700

    create appbar, post and form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,43 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import galaxy from "./images/galaxy.jpg";
+import useStyles from "./styles";
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} varaint="h1" align="center">
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={galaxy}
+          alt="memories"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
