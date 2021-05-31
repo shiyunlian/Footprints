f82d53ef9e5e87a3ed6f28722c15d0c6df2d18d0
shiyunlian <shiyunlian00@gmail.com>
2021-05-31 14:15:58 -0700

    adjust form styling

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
-import FileBase from "react-file-base64";
 import { useDispatch, useSelector } from "react-redux";
+import FileBase from "react-file-base64";
+
 import useStyles from "./styles";
 import { createPost, updatePost } from "../../actions/posts";
 
-const Form = (currentId, setCurrentId) => {
+const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({
     creator: "",
     title: "",
@@ -14,27 +15,17 @@ const Form = (currentId, setCurrentId) => {
     selectedFile: "",
   });
   const post = useSelector((state) =>
-    currentId ? state.posts.find((p) => p._id === currentId) : null
+    currentId ? state.posts.find((message) => message._id === currentId) : null
   );
-  const classes = useStyles();
   const dispatch = useDispatch();
+  const classes = useStyles();
+
   useEffect(() => {
     if (post) setPostData(post);
   }, [post]);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    /*if there is an existing current id, update the post, if not, create a post */
-    if (currentId) {
-      dispatch(updatePost(currentId, postData));
-    } else {
-      dispatch(createPost(postData));
-    }
-    clear();
-  };
 
   const clear = () => {
-    setCurrentId(null);
+    setCurrentId(0);
     setPostData({
       creator: "",
       title: "",
@@ -42,6 +33,18 @@ const Form = (currentId, setCurrentId) => {
       tags: "",
       selectedFile: "",
     });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (currentId === 0) {
+      dispatch(createPost(postData));
+      clear();
+    } else {
+      dispatch(updatePost(currentId, postData));
+      clear();
+    }
   };
 
   return (
@@ -52,12 +55,12 @@ const Form = (currentId, setCurrentId) => {
         className={`${classes.root} ${classes.form}`}
         onSubmit={handleSubmit}
       >
-        <Typography varient="h6">
-          {currentId ? "Editing" : "Creating"} a memory
+        <Typography variant="h6">
+          {currentId ? `Editing "${post.title}"` : "Creating a Memory"}
         </Typography>
         <TextField
           name="creator"
-          varient="outlined"
+          variant="outlined"
           label="Creator"
           fullWidth
           value={postData.creator}
@@ -67,7 +70,7 @@ const Form = (currentId, setCurrentId) => {
         />
         <TextField
           name="title"
-          varient="outlined"
+          variant="outlined"
           label="Title"
           fullWidth
           value={postData.title}
@@ -75,9 +78,11 @@ const Form = (currentId, setCurrentId) => {
         />
         <TextField
           name="message"
-          varient="outlined"
+          variant="outlined"
           label="Message"
           fullWidth
+          multiline
+          rows={4}
           value={postData.message}
           onChange={(e) =>
             setPostData({ ...postData, message: e.target.value })
@@ -85,8 +90,8 @@ const Form = (currentId, setCurrentId) => {
         />
         <TextField
           name="tags"
-          varient="outlined"
-          label="Tags"
+          variant="outlined"
+          label="Tags (coma separated)"
           fullWidth
           value={postData.tags}
           onChange={(e) =>
@@ -94,30 +99,33 @@ const Form = (currentId, setCurrentId) => {
           }
         />
         <div className={classes.fileInput}>
-          <FileBase>
-            type="file" multiple={false} onDone=
-            {({ base64 }) => setPostData({ ...postData, selectedFile: base64 })}
-          </FileBase>
-          <Button
-            className={classes.buttonSubmit}
-            varient="container"
-            color="primary"
-            size="large"
-            type="submit"
-            fullWidth
-          >
-            Submit
-          </Button>
-          <Button
-            varient="contained"
-            color="secondary"
-            size="large"
-            onClick={clear}
-            fullWidth
-          >
-            Clear
-          </Button>
+          <FileBase
+            type="file"
+            multiple={false}
+            onDone={({ base64 }) =>
+              setPostData({ ...postData, selectedFile: base64 })
+            }
+          />
         </div>
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          color="primary"
+          size="large"
+          type="submit"
+          fullWidth
+        >
+          Submit
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          size="small"
+          onClick={clear}
+          fullWidth
+        >
+          Clear
+        </Button>
       </form>
     </Paper>
   );
