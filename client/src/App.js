9a8bed2096800e95a61145e595887bf8f7803af0
shiyunlian <shiyunlian00@gmail.com>
2021-05-31 14:34:44 -0700

    adjust container

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import { useDispatch } from "react-redux";
-import { getPosts } from "./actions/posts";
+
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
-import galaxy from "./images/galaxy.jpg";
+import { getPosts } from "./actions/posts";
 import useStyles from "./styles";
+import memories from "./images/galaxy.jpg";
 
 const App = () => {
-  const [currentId, setCurrentId] = useState(null);
-  const classes = useStyles();
+  const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
+  const classes = useStyles();
 
   useEffect(() => {
     dispatch(getPosts());
   }, [currentId, dispatch]);
 
   return (
-    <Container maxidth="lg">
+    <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} varaint="h1" align="center">
+        <Typography className={classes.heading} variant="h2" align="center">
           Memories
         </Typography>
-        <img
-          className={classes.image}
-          src={galaxy}
-          alt="memories"
-          height="60"
-        />
+        <img className={classes.image} src={memories} alt="icon" height="60" />
       </AppBar>
       <Grow in>
         <Container>
@@ -41,7 +37,7 @@ const App = () => {
               <Posts setCurrentId={setCurrentId} />
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} />
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
             </Grid>
           </Grid>
         </Container>
