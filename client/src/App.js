88d1485b919da2e126574ebfcdb57d556db75b1e
shiyunlian <shiyunlian00@gmail.com>
2021-06-01 00:05:36 -0700

    modify heading

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import { getPosts } from "./actions/posts";
 import useStyles from "./styles";
-import memories from "./images/galaxy.jpg";
+import camera from "./images/camera.jpg";
 
 const App = () => {
   const [currentId, setCurrentId] = useState(0);
@@ -21,9 +21,9 @@ const App = () => {
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography className={classes.heading} variant="h2" align="center">
-          Memories
+          Footprints 足迹
         </Typography>
-        <img className={classes.image} src={memories} alt="icon" height="60" />
+        <img className={classes.image} src={camera} alt="icon" height="60" />
       </AppBar>
       <Grow in>
         <Container>
