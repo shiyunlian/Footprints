496601829a3ac5d4b44f94627527558a5c1ba93c
shiyunlian <shiyunlian00@gmail.com>
2021-08-14 20:24:29 -0700

    modify index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 import express from "express";
-import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
 const app = express();
 dotenv.config();
@@ -12,6 +12,7 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
