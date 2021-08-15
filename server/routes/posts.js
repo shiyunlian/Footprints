fee9ac8197fa0125082313261c858c0534de3bd1
shiyunlian <shiyunlian00@gmail.com>
2021-08-14 20:22:07 -0700

    import auth and modify posts routes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,22 +1,21 @@
 import express from "express";
 import {
   getPosts,
-  getPost,
   createPost,
   updatePost,
   deletePost,
   likePost,
 } from "../controllers/posts.js";
-
+import auth from "../middleWare/auth.js";
 // create a router object from express
 const router = express.Router();
 
 // root site is http://localhost:5000/posts
 // add HTTP method routes
 router.get("/", getPosts);
-router.post("/", createPost);
-router.get("/:id", getPost);
-router.patch("/:id", updatePost);
-router.delete("/:id", deletePost);
-router.patch("/:id/likePost", likePost);
+router.post("/", auth, createPost);
+router.patch("/:id", auth, updatePost);
+router.delete("/:id", auth, deletePost);
+router.patch("/:id/likePost", auth, likePost);
+
 export default router;
