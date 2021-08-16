db3b76af15cb93cf0de36c02a183dd200ce47dc0
shiyunlian <shiyunlian00@gmail.com>
2021-08-15 20:09:22 -0700

    create reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-
+import auth from "./auth";
 import posts from "./posts";
 
-export const reducers = combineReducers({ posts });
+export const reducers = combineReducers({ posts, auth });
